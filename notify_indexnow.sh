12a066a07396d485e621b8bbfcd4af24a6773a7d
gab <gab@GMBP16M1.local>
2026-02-27 11:06:53 +1000

    update

--- notify_indexnow.sh
+++ notify_indexnow.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-SITEMAP_URL="https://angliport.com/sitemap.xml"
+SITEMAP_URL="http://angliport.com/sitemap.xml"
 HOST="angliport.com"
-KEY="f25f2ae156e844228b12042703fb53ed"
-KEY_LOCATION="https://angliport.com/f25f2ae156e844228b12042703fb53ed.txt"
+KEY="fb2ec9f5dffe4263a6bfaa78452ea99c"
+KEY_LOCATION="http://angliport.com/fb2ec9f5dffe4263a6bfaa78452ea99c.txt"
 INDEXNOW_ENDPOINT="https://api.indexnow.org/IndexNow"
 
  # Extract URLs from sitemap.xml
 # URLS=$(curl -s "$SITEMAP_URL" | sed -n 's:.*<loc>\(.*\)</loc>.*:\1:p' | awk '{print "\"" $0 "\""}' | paste -sd "," -)
-URLS=$(
-  curl -s "$SITEMAP_URL" \
-    | sed -n 's:.*<[^>]*loc>\(.*\)</[^>]*loc>.*:\1:p' \
-    | grep '^https://angliport.com' \
-    | awk '{print "\"" $0 "\""}' \
-    | paste -sd "," -
-)
+URLS="http://angliport.com"
 
 ECHO "Extracted URLs: $URLS"
 # Build JSON payload
-JSON_PAYLOAD="{\"host\":\"$HOST\",\"key\":\"$KEY\",\"keyLocation\":\"$KEY_LOCATION\",\"urlList\":[ $URLS ]}"
+JSON_PAYLOAD="{\"host\":\"$HOST\",\"key\":\"$KEY\",\"keyLocation\":\"$KEY_LOCATION\",\"urlList\":[ \"$URLS\" ]}"
 
+ECHO "Extracted Payload: $JSON_PAYLOAD"
 # Send POST request
 curl -X POST "$INDEXNOW_ENDPOINT" \
      -H "Content-Type: application/json; charset=utf-8" \
      -d "$JSON_PAYLOAD"
+
+
+
+ECHO "Done"
